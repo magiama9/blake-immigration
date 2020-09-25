49aec47266ffe89dfa99741cf74b25abfb598a4e
Sam Randels <samrandels@gmail.com>
2020-09-24 22:03:59 -0400

    add facebook link to footer

--- gatsby-card-layout/src/components/Footer.js
+++ gatsby-card-layout/src/components/Footer.js
@@ -1,18 +1,26 @@
 import React from 'react'
 
-const Footer = (props) => (
-    <footer id="footer">
-        <div className="inner">
-            <ul className="icons">
-                <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-            </ul>
-            <ul className="copyright">
-                <li>&copy; Blake Immigration Law</li><li>Design: <a href="https://samrandels.io">Sam Randels</a></li>
-            </ul>
-        </div>
-    </footer>
+const Footer = props => (
+  <footer id="footer">
+    <div className="inner">
+      <ul className="icons">
+        <li>
+          <a
+            href="http://www.facebook.com/blakeimmigration"
+            className="icon alt fa-facebook"
+          >
+            <span className="label">Facebook</span>
+          </a>
+        </li>
+      </ul>
+      <ul className="copyright">
+        <li>&copy; Blake Immigration Law 2020</li>
+        <li>
+          Design: <a href="https://samrandels.io">Sam Randels</a>
+        </li>
+      </ul>
+    </div>
+  </footer>
 )
 
 export default Footer
